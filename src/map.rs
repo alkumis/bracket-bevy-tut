093c5f719c3381216cccf7b6a7d920f2c46d3595
riqsha <shariq.rafeek@gmail.com>
2025-10-11 14:08:09 +0100

    Removed unnecessary mutability from shout_insults system

--- src/map.rs
+++ src/map.rs
@@ -3,7 +3,7 @@ use crate::rect::Rect;
 use bevy::{platform::collections::HashSet, prelude::*};
 use bracket_lib::{
     bevy::*,
-    prelude::{Algorithm2D, BaseMap},
+    prelude::{Algorithm2D, BaseMap, DistanceAlg, SmallVec},
     random::RandomNumberGenerator,
 };
 use std::cmp::{max, min};
@@ -110,6 +110,37 @@ impl Map {
 
         map
     }
+
+    fn is_exit_valid(&self, x: i32, y: i32) -> bool {
+        if x < 1 || x > self.width - 1 || y < 1 || y > self.height - 1 {
+            return false;
+        }
+        let idx = self.xy_idx(x, y);
+        self.tiles[idx as usize] != TileType::Wall
+    }
+
+    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
+        let mut exits = SmallVec::new();
+        let x = idx as i32 % self.width;
+        let y = idx as i32 / self.width;
+        let w = self.width as usize;
+
+        // Cardinal directions
+        if self.is_exit_valid(x - 1, y) {
+            exits.push((idx - 1, 1.0))
+        };
+        if self.is_exit_valid(x + 1, y) {
+            exits.push((idx + 1, 1.0))
+        };
+        if self.is_exit_valid(x, y - 1) {
+            exits.push((idx - w, 1.0))
+        };
+        if self.is_exit_valid(x, y + 1) {
+            exits.push((idx + w, 1.0))
+        };
+
+        exits
+    }
 }
 
 impl Algorithm2D for Map {
@@ -121,6 +152,13 @@ impl Algorithm2D for Map {
 impl BaseMap for Map {
     fn is_opaque(&self, idx: usize) -> bool {
         self.tiles[idx] == TileType::Wall
+    }
+
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
+        let w = self.width as usize;
+        let p1 = Point::new(idx1 % w, idx1 / w);
+        let p2 = Point::new(idx2 % w, idx2 / w);
+        DistanceAlg::Pythagoras.distance2d(p1, p2)
     }
 }
 
